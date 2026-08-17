24771fcd0b85f9d826dc6dcac1b16c4f720130bc
Ana Clara Amaral <anaclara.bany@gmail.com>
2026-08-17 16:50:12 -0300

    first commit

---
+++ src/main/java/com/ifast/ifast_backend/IfastBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ifast.ifast_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfastBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfastBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifast/ifast_backend/IfastBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifast.ifast_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfastBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
