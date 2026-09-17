faee7c185ff1e251c5343693650b20bdf526ec8f
Pedro Inácio <pedroramos120606@gmail.com>
2026-09-17 16:36:49 -0300

    feat: melhora interceptor de erro da api e lida com erro geral

--- frontend/src/shared/api.ts
+++ frontend/src/shared/api.ts
@@ -1,5 +1,6 @@
 import { getAccessToken } from "@auth/authStorage";
 import axios from "axios";
+import { useErrorStore } from "./errorStore";
 
 export const api = axios.create({ baseURL: import.meta.env.VITE_API_URL });
 
@@ -14,7 +15,29 @@ api.interceptors.request.use((config) => {
 api.interceptors.response.use(
   (response) => response,
   async (error) => {
+    if (!axios.isAxiosError(error)) {
+      return Promise.reject(error);
+    }
+
+    if (axios.isCancel(error)) {
+      return Promise.reject(error);
+    }
+
+    if (import.meta.env.DEV) {
+      console.group(`[Axios Interceptor Error] ${error.config?.url}`);
+      console.error("Status:", error.response?.status ?? "Sem resposta");
+      console.error("Payload/Data:", error.response?.data);
+      console.groupEnd();
+    }
+
     const status = error.response?.status;
+
+    if (!error.response || (status && status >= 500)) {
+      useErrorStore
+        .getState()
+        .setGlobalError("Ocorreu um erro ao conectar aos nossos servidores. Por favor, tente novamente mais tarde");
+      return Promise.reject(error);
+    }
 
     if (status === 401) {
       const { useAuthStore } = await import("@auth/authStore");
@@ -22,16 +45,16 @@ api.interceptors.response.use(
     }
 
     const data = error.response?.data;
-    const message = data?.message ?? "Erro de conexão com o servidor";
+    const message = data?.message ?? "Ocorreu um erro";
 
     return Promise.reject(new ApiError(message, status));
   },
 );
 
 export class ApiError extends Error {
-  public status: number;
+  public status: number | undefined;
 
-  constructor(message: string, status: number) {
+  constructor(message: string, status: number | undefined) {
     super(message);
     this.name = "ApiError";
     this.status = status;
