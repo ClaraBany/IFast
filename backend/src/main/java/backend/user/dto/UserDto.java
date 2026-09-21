17aee5ecdc9900adfe0caa83d24675c9ec42d741
Pedro Inácio <pedroramos120606@gmail.com>
2026-09-21 10:09:26 -0300

    feat(back): adiciona campos de foto, contato e endereço ao UserDto

--- backend/src/main/java/backend/user/dto/UserDto.java
+++ backend/src/main/java/backend/user/dto/UserDto.java
@@ -1,6 +1,7 @@
-package backend.auth.dto;
+package backend.user.dto;
 
-import backend.auth.User;
+import backend.user.Address;
+import backend.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,10 +13,16 @@ public class UserDto {
     private Long id;
     private String name;
     private String email;
+    private String pictureUrl;
+    private String phoneNumber;
+    private Address address;
 
     public UserDto(User user) {
         this.id = user.getId();
         this.name = user.getName();
         this.email = user.getEmail();
+        this.pictureUrl = user.getPictureUrl();
+        this.phoneNumber = user.getPhoneNumber();
+        this.address = user.getAddress();
     }
 }   
