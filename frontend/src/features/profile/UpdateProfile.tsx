d36508029e4eb743c93994fe438082c924f83bf3
Pedro Inácio <pedroramos120606@gmail.com>
2026-09-23 21:20:58 -0300

    feat(front): editar perfil

--- frontend/src/features/profile/UpdateProfile.tsx
+++ frontend/src/features/profile/UpdateProfile.tsx
@@ -3,8 +3,35 @@ import BackButton from "@shared/components/BackButton";
 import { useForm } from "react-hook-form";
 import z from "zod";
 import { updateProfileSchema } from "./types";
+import { updateProfile } from "./profileService";
+import { LoaderCircle } from "lucide-react";
+import { useAuthStore } from "@auth/authStore";
+import { useNavigate } from "react-router";
 
 export default function UpdateProfile() {
+  const user = useAuthStore((state) => state.user);
+  const setUser = useAuthStore((state) => state.setUser);
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<z.infer<typeof updateProfileSchema>>({
+    resolver: zodResolver(updateProfileSchema),
+    mode: "onTouched",
+    defaultValues: {
+      name: user?.name,
+      address: user?.address ?? "",
+      phoneNumber: user?.phoneNumber ?? "",
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof updateProfileSchema>) => {
+    setUser(await updateProfile(data));
+    navigate(-1);
+  };
+
   const formatPhoneNumber = (value: string) => {
     return value
       .replace(/\D/g, "")
@@ -12,12 +39,6 @@ export default function UpdateProfile() {
       .replace(/(\d{2})(\d)/, "($1) $2")
       .replace(/(\d{5})(\d)/, "$1-$2");
   };
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<z.infer<typeof updateProfileSchema>>({ resolver: zodResolver(updateProfileSchema), mode: "onTouched" });
 
   const { onChange, ...rest } = register("phoneNumber");
 
@@ -27,57 +48,60 @@ export default function UpdateProfile() {
         <div className="flex-center gap-2.5">
           <BackButton />
 
-          <h2>Perfil</h2>
+          <h2>Editar Perfil</h2>
         </div>
       </section>
 
-      <section className="flex-column gap-7.5 rounded-[20px] bg-white p-4">
-        <div className="field">
-          <label htmlFor="name">Nome</label>
-          <input
-            {...register("name")}
-            aria-invalid={errors.name ? "true" : "false"}
-            type="text"
-            placeholder="Digite seu nome"
-            id="name"
-            autoComplete="name"
-          ></input>
-          {errors.name && <span>{errors.name.message}</span>}
-        </div>
+      <form onSubmit={handleSubmit(onSubmit)} className="flex-column flex-1 gap-7.5">
+        <section className="flex-column gap-7.5 rounded-[20px] bg-white p-4">
+          <div className="field">
+            <label htmlFor="name">Nome</label>
+            <input
+              {...register("name")}
+              aria-invalid={errors.name ? "true" : "false"}
+              type="text"
+              placeholder="Digite seu nome"
+              id="name"
+              autoComplete="name"
+            ></input>
+            {errors.name && <span>{errors.name.message}</span>}
+          </div>
 
-        <div className="field">
-          <label htmlFor="name">Endereço</label>
-          <input
-            {...register("address")}
-            aria-invalid={errors.address ? "true" : "false"}
-            type="text"
-            placeholder="Digite seu endereço"
-            id="address"
-          ></input>
-          {errors.address && <span>{errors.address.message}</span>}
-        </div>
+          <div className="field">
+            <label htmlFor="address">Endereço</label>
+            <input
+              {...register("address")}
+              aria-invalid={errors.address ? "true" : "false"}
+              type="text"
+              placeholder="Digite seu endereço"
+              id="address"
+            ></input>
+            {errors.address && <span>{errors.address.message}</span>}
+          </div>
 
-        <div className="field">
-          <label htmlFor="phone-number">Telefone</label>
-          <input type="tel" placeholder="Digite seu telefone" id="phone-number"></input>
-          <label htmlFor="phoneNumber">Telefone</label>
-          <input
-            {...rest}
-            aria-invalid={errors.phoneNumber ? "true" : "false"}
-            type="tel"
-            placeholder="Digite seu telefone"
-            id="phoneNumber"
-            autoComplete="tel"
-            onChange={(e) => {
-              e.target.value = formatPhoneNumber(e.target.value);
-              onChange(e);
-            }}
-          ></input>
-          {errors.phoneNumber && <span>{errors.phoneNumber.message}</span>}
-        </div>
-      </section>
+          <div className="field">
+            <label htmlFor="phoneNumber">Telefone</label>
+            <input
+              {...rest}
+              aria-invalid={errors.phoneNumber ? "true" : "false"}
+              type="tel"
+              placeholder="Digite seu telefone"
+              id="phoneNumber"
+              autoComplete="tel"
+              onChange={(e) => {
+                e.target.value = formatPhoneNumber(e.target.value);
+                onChange(e);
+              }}
+            ></input>
+            {errors.phoneNumber && <span>{errors.phoneNumber.message}</span>}
+          </div>
+        </section>
 
-      <button className="btn btn-lg mt-auto bg-primary">Salvar Mudanças</button>
+        <button disabled={isSubmitting} type="submit" className="btn btn-lg mt-auto bg-primary">
+          {isSubmitting && <LoaderCircle className="animate-spin" />}
+          Salvar Mudanças
+        </button>
+      </form>
     </main>
   );
 }
--- frontend/src/shared/components/SideMenu.tsx
+++ frontend/src/shared/components/SideMenu.tsx
@@ -19,7 +19,7 @@ export default function SideMenu({ open, setOpen }: SideMenuProps) {
         <Drawer.Overlay className="fixed inset-0 bg-black/40 backdrop-blur-[1.5px]" />
         <Drawer.Content className="fixed top-0 bottom-0 left-0 my-20 flex w-[320px] rounded-e-2xl bg-white">
           <div className="flex-column w-full items-start gap-5 overflow-y-auto p-6">
-            <Link to={""} className="flex-center gap-2.5" onClick={() => setOpen(false)}>
+            <Link to={`/user/${user!.id}`} onClick={() => setOpen(false)} className="flex-center gap-2.5">
               <CircleUserRound size={55} strokeWidth={1} />
               <h3>{user?.name ?? "Usuário"}</h3>
             </Link>
